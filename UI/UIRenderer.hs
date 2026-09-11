7e1a4c5c1a396a6fe83e73685157011d64fc17c6
Zhi Ti Kuan <zhitikuan@gmail.com>
2026-09-11 15:49:19 +0800

    Fix: chunking issue with renderer

--- UI/UIRenderer.hs
+++ UI/UIRenderer.hs
@@ -26,12 +26,13 @@ boldGrid = "\ESC[1m"
 -- Render top-level UI
 renderUI :: UIState -> IO ()
 renderUI st = do
+  putStr hideCursor
   putStr clearScreen
   putStrLn (boldGrid ++ "=== HASKELL TERMINAL SUDOKU ===" ++ resetStyle)
   putStrLn ""
   putStr (formatBoard (grid st) (cursor st))
   putStrLn ""
-  putStrLn ("Status: " ++ status st)
+  putStrLn ("Status: " ++ status st ++ resetStyle)
   hFlush stdout
 
 -- Render grid lines and highlight active cursor
@@ -39,8 +40,15 @@ formatBoard :: Grid Cell -> (Int, Int) -> String
 formatBoard g (curR, curC) = unlines (concatMap renderRow [0..8])
   where
     renderRow r = 
-      let lineStr = concat [ formatCell r c (g !! (r * 9 + c)) | c <- [0..8] ]
-          rowOut  = insertBoxSeparators lineStr
+      let -- Generate a list of 9 formatted cell strings
+          cellsInRow = [ formatCell r c (g !! (r * 9 + c)) | c <- [0..8] ]
+          
+          -- Safely chunk into 3 groups of 3 cells each
+          box1 = concat (take 3 cellsInRow)
+          box2 = concat (take 3 (drop 3 cellsInRow))
+          box3 = concat (drop 6 cellsInRow)
+          
+          rowOut = box1 ++ "|" ++ box2 ++ "|" ++ box3
       in if r > 0 && r `mod` 3 == 0
            then ["------+-------+------", rowOut]
            else [rowOut]
@@ -53,8 +61,3 @@ formatBoard g (curR, curC) = unlines (concatMap renderRow [0..8])
       in if isCursor
            then bgHighlight ++ " " ++ digitChar ++ " " ++ resetStyle
            else " " ++ digitChar ++ " "
-
-    insertBoxSeparators rowStr = 
-      -- Formats 3 columns per box group
-      let chunks = [ take 9 rowStr, take 9 (drop 9 rowStr), drop 18 rowStr ]
-      in head chunks ++ "|" ++ (chunks !! 1) ++ "|" ++ (chunks !! 2)
